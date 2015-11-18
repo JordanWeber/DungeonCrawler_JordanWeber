2fb60fed3479340c436af996300bad63fdb0d846
D22 <JordanWeber@ninja19.d22.lan>
2015-11-18 11:23:47 -0600

    Game

---
+++ NetBeansProjects/Weber_3_InteractiveFiction/src/weber_3_interactivefiction/Weber_3_InteractiveFiction.java
@@ -0,0 +1,19 @@
+package weber_3_interactivefiction;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author JordanWeber
+ */
+public class Weber_3_InteractiveFiction {
+    static Scanner sc = new Scanner(System.in);
+    static Random rand = new Random();
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
